7b04c01e8ea3428a7aecfc503e211ac8b70b78e7
OldValencia <OldValencia@users.noreply.github.com>
2025-07-26 15:08:22 +0000

    deploy: f69c3abef990c18c4a87526cfa534fbe46dd516b

--- scripts/config.template.js
+++
@@ -1,2 +0,0 @@
-const WEATHER_API_KEY = "__WEATHER_API_KEY__";
-const PIXABAY_API_KEY = "__PIXABAY_API_KEY__";
--- scripts/effects/effects-main.js
+++
@@ -1,8 +0,0 @@
-function disableDynamicBackground() {
-    backgroundLayer.innerHTML = "";
-    backgroundLayer.style.backgroundImage = "";
-    backgroundLayer.style.backgroundSize = "";
-    backgroundLayer.style.backgroundRepeat = "";
-    backgroundLayer.style.backgroundPosition = "";
-    document.body.style.backgroundImage = "";
-}
